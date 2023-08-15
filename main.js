1c0c03460869eb0888cdd7e60f964d7fb594785f
DipankarSethi3012 <123314024+DipankarSethi3012@users.noreply.github.com>
2023-08-15 11:05:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Front-End-developer","CPP Programmer","Table-Tennis Player","Chess-Player"],
+    typeSpeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+});
